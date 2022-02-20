55f819b2a28f68ddbcb83ee58194a35fcf6ee17c
Massimiliano Ravelli <massimiliano.ravelli@gmail.com>
2022-02-20 21:05:34 +0100

    fix backendUrl attribute

--- ui/lib/app.dart
+++ ui/lib/app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
-  const App({String? backendUrl = '', Key? key}) : super(key: key);
+  final String backendUrl;
+
+  const App({this.backendUrl = '', Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
